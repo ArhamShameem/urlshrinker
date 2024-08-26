a80077dff0acd9ec306df1359772b5b390c61fcd
ArhamShameem <golushameem001@gmail.com>
2024-08-26 21:11:13 +0530

    seventh commit

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -1,44 +1,145 @@
-import React from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import {Button} from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { UrlState } from "@/context";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card } from "./ui/card";
+import {Input} from "@/components/ui/input";
+import {Card} from "./ui/card";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
 import Error from "./error";
+import * as yup from "yup";
+import useFetch from "@/hooks/use-fetch";
+import {createUrl} from "@/db/apiUrls";
+import {BeatLoader} from "react-spinners";
+import {UrlState} from "@/context";
+import {QRCode} from "react-qrcode-logo";
 
+export function createLink() {
+  const {user} = UrlState();
 
-const createLink = () => {
-  const { user } = UrlState();
   const navigate = useNavigate();
+  const ref = useRef();
+
   let [searchParams, setSearchParams] = useSearchParams();
-  const longlink = searchParams.get("createNew");
+  const longLink = searchParams.get("createNew");
+
+  const [errors, setErrors] = useState({});
+  const [formValues, setFormValues] = useState({
+    title: "",
+    longUrl: longLink ? longLink : "",
+    customUrl: "",
+  });
+
+  const schema = yup.object().shape({
+    title: yup.string().required("Title is required"),
+    longUrl: yup
+      .string()
+      .url("Must be a valid URL")
+      .required("Long URL is required"),
+    customUrl: yup.string(),
+  });
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const {
+    loading,
+    error,
+    data,
+    fn: fnCreateUrl,
+  } = useFetch(createUrl, {...formValues, user_id: user.id});
+
+  useEffect(() => {
+    if (error === null && data) {
+      navigate(`/link/${data[0].id}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, data]);
+
+  const createNewLink = async () => {
+    setErrors([]);
+    try {
+      await schema.validate(formValues, {abortEarly: false});
+
+      const canvas = ref.current.canvasRef.current;
+      console.log(canvas)
+      const blob = await new Promise((resolve) => canvas.toBlob(resolve));
+
+      await fnCreateUrl(blob);
+    } catch (e) {
+      const newErrors = {};
+
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger>
+    <Dialog
+      defaultOpen={longLink}
+      onOpenChange={(res) => {
+        if (!res) setSearchParams({});
+      }}
+    >
+      <DialogTrigger asChild>
         <Button variant="destructive">Create New Link</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
         </DialogHeader>
-        <Input id="title" placeholder="Short Link's Title"/>
-        {/* <Error message={"some error"}/> */}
-        <Input id="title" placeholder="Enter your Long Url"/>
-        {/* <Error message={"some error"}/> */}
-        <Card className="p-2">trimrr.in</Card>/
-        <Input id="title" placeholder="Custom Link (optional)"/>
-        {/* <Error message={"some error"}/> */}
+        {formValues?.longUrl && (
+          <QRCode ref={ref} size={250} value={formValues?.longUrl} />
+        )}
+
+        <Input
+          id="title"
+          placeholder="Short Link's Title"
+          value={formValues.title}
+          onChange={handleChange}
+        />
+        {errors.title && <Error message={errors.title} />}
+        <Input
+          id="longUrl"
+          placeholder="Enter your Loooong URL"
+          value={formValues.longUrl}
+          onChange={handleChange}
+        />
+        {errors.longUrl && <Error message={errors.longUrl} />}
+        <div className="flex items-center gap-2">
+          <Card className="p-2">trimrr.in</Card> /
+          <Input
+            id="customUrl"
+            placeholder="Custom Link (optional)"
+            value={formValues.customUrl}
+            onChange={handleChange}
+          />
+        </div>
+        {error && <Error message={errors.message} />}
+        <DialogFooter className="sm:justify-start">
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={createNewLink}
+            disabled={loading}
+          >
+            {loading ? <BeatLoader size={10} color="white" /> : "Create"}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
-};
-
+}
 export default createLink;
--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,5 +1,6 @@
 import supabase from "./supabase";
 import supabaseUrl from "./supabase";
+import UAParser from "ua-parser-js";
 
 export async function getUrls(user_id) {
   const { data, error } = await supabase
@@ -20,8 +21,11 @@ export async function deleteUrl(id) {
   }
   return data;
 }
-export async function createUrl({title, longUrl,customUrl,user_id},qrcode) {
-  const short_url=Math.random().toString(36).substring(2,6)
+export async function createUrl(
+  { title, longUrl, customUrl, user_id },
+  qrcode
+) {
+  const short_url = Math.random().toString(36).substring(2, 6);
 
   const filename = `qr-${short_url}`;
   const { error: storageError } = await supabase.storage
@@ -30,15 +34,51 @@ export async function createUrl({title, longUrl,customUrl,user_id},qrcode) {
 
   if (storageError) throw new Error(storageError.message);
 
-const qr=`${supabaseUrl}/storage/v1/object/public/qrs/${filename}`
-  const { data, error } = await supabase.from("urls").insert([
-    {
-      title, original_url:longUrl,custom_url:customUrl||null,user_id,short_url,qr,
-    },
-  ]).select()
+  const qr = `${"https://sbfqyfalktpbdwxnypch.supabase.co"}/storage/v1/object/public/qrs/${filename}`;
+  const { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        original_url: longUrl,
+        custom_url: customUrl || null,
+        user_id,
+        short_url,
+        qr,
+      },
+    ])
+    .select();
   if (error) {
     console.log(error.message);
     throw new Error("Unable to load Urls");
   }
   return data;
 }
+export async function getLongUrl(id) {
+  const { data, error } = await (
+    await supabase.from("urls").select("id,original_url")
+  ).error(`short_url.eq.${id},custom_url.eq.${id}`);
+
+  if (error) {
+    console.log(error.message);
+    throw new Error("Unable to load Urls");
+  }
+  return data;
+}
+const parser = new UAParser();
+export const storeClicks = async ({ id, original_url }) => {
+  try {
+    const res = parser.getResult();
+    const device = res.type || "desktop";
+
+    const response = await fetch("https://ipapi.co/json");
+    const { city, country_name: country_name } = await response.json();
+    await supabase.from("clicks").insert({
+      url_id: id,
+      city: city,
+      country: country,
+      device: device,
+    });
+    window.location.href = original_url;
+  } catch (error) {}
+};
